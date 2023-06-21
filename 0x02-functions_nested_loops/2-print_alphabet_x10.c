0b4373efc86df1676b00fde5f7c353dc4b01b897
Daniel <daniellovelyyeboah@yahoo.com>
2023-06-20 22:53:23 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "maih.h"
+/**
+ * main - function that prints 10times the alphabet in lowercase
+ * Return: 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (a = 1; a <= 10 ; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar("\n");
+	}
+	return (0);
+}
+
+
