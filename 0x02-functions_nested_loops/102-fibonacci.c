07117d4508549e5927378dd549fa92d3258f36f3
Daniel <daniellovelyyeboah@yahoo.com>
2023-06-21 22:56:46 -0700

    In computer class

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include < studio.h>
+
+/**
+ * main - program that prints the first 50 Fibonacci numbers
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+	int a = 0;
+	long b = 1, c = 2;
+
+	while (a < 50)
+	{
+		if (a == 0)
+			printf("%ld", b);
+		else if (a == 1)
+			printf(", %ld", c);
+		else
+		{
+			c += b;
+			b = c - b;
+			printf(", %ld", c);
+		}
+		++a;
+	}
+	printf("\n");
+	return (0);
+}
+
